32c629ae98f6c239e28a0a72ff2a308343d101c4
AnAr854 <83339747+AnAr854@users.noreply.github.com>
2024-04-12 18:27:26 +0300

    Database connection added

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
+require('dotenv').config()
 const exphbs = require('express-handlebars');
+
+//Database
+const dbURI = 'mongodb+srv://' + process.env.DBUSERNAME + ':' + process.env.DBPASSWORD + '@' + process.env.CLUSTER + '.mongodb.net/' + process.env.DB + '?retryWrites=true&w=majority&appName=Cluster0'
+
+//Wait for database connection
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log('Database access succesful!')
+        const PORT = process.env.PORT || 3000
+        app.listen(PORT, () => console.log('Listening port: ' + PORT))
+    })
+    .catch((error) => {
+        console.log('Error occurred connecting to database: ' + error)
+    })
 
 const app = express();
 app.set('view engine', 'handlebars');
@@ -18,5 +34,3 @@ app.use((req, res, next) => {
     res.status(404).send("Sorry, we could not find the content.");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
