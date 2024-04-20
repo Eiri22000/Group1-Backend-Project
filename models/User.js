ef690b0bb832d2c9e3f71633c2eed64919b474d0
Eiri22000 <113633681+Eiri22000@users.noreply.github.com>
2024-04-20 17:13:12 +0300

    More updates to gardener page

--- models/User.js
+++ models/User.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+    _id: {
+        String
+    },
     name: {
         type: String,
         required: true
